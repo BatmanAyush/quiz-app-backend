747bb7d16d4f3ec52844b5b14944afc87bb84142
ayushsharma <asbro3886@gmail.com>
2025-10-30 14:23:01 +0530

    oauth fixes

--- src/main/java/com/revision/tool/config/SecurityConfig.java
+++ src/main/java/com/revision/tool/config/SecurityConfig.java
@@ -22,9 +22,10 @@ public class SecurityConfig {
 
     @Bean @Order(1)
     public SecurityFilterChain oauthChain(HttpSecurity http) throws Exception {
+
         http
                 .securityMatcher("/login/oauth2/**","/oauth2/**","/api/auth/**")
-                .cors(c -> {}) // keep
+                .cors(c -> {c.configurationSource(corsConfigurationSource());}) // keep
                 .authorizeHttpRequests(a -> a.anyRequest().permitAll())
                 .oauth2Login(o -> o.defaultSuccessUrl("/api/auth/oauth-success", true));
         // CSRF yahan stateful flows ke liye theek rahega
