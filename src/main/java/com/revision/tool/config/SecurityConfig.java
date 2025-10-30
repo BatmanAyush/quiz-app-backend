0df31450989e3ef8d0e2a981af386f1324101017
ayushsharma <asbro3886@gmail.com>
2025-10-30 10:47:05 +0530

    frontend cors adding

--- src/main/java/com/revision/tool/config/SecurityConfig.java
+++ src/main/java/com/revision/tool/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         var cfg = new org.springframework.web.cors.CorsConfiguration();
-        cfg.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
+        cfg.setAllowedOrigins(java.util.List.of("http://localhost:5173","https://quiz-app-frontend-taupe.vercel.app"));
         cfg.setAllowedMethods(java.util.List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(java.util.List.of("Authorization","Content-Type"));
         cfg.setAllowCredentials(true);
